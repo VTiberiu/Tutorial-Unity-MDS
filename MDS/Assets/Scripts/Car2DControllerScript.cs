f5b39a9668b4848dde718247666e87e7981d31e8
VTiberiu <voroneanu.tiberiu@gmail.com>
2017-04-20 14:42:36 +0300

    20.04.2017
    Am facut cateva mici modificari.

---
+++ MDS/Assets/Scripts/Car2DControllerScript.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Car2DControllerScript : MonoBehaviour {
+
+	public float speedForce =100f;
+	public float torquqForce = 100f;
+
+	// Use this for initialization
+	void Start () {
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+
+		Rigidbody2D rb = GetComponent<Rigidbody2D> ();
+		if (Input.GetButton ("Accelerate")) {
+			rb.AddForce (transform.up * speedForce);
+		} else if (Input.GetButton ("Break")) {
+			rb.AddForce (-transform.up * speedForce);
+		}
+
+		rb.AddTorque (Input.GetAxis ("Horizontal") * torquqForce);
+
+
+
+
+	}
+}
