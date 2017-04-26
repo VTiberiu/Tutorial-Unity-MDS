fd6c7721eecf11c676161b0a8a9fe97686210694
Alex-Badea <Alex-Badea@github.com>
2017-04-26 16:07:24 +0300

    modificat controlul mașinii

--- MDS/Assets/Scripts/Car2DControllerScript.cs
+++ MDS/Assets/Scripts/Car2DControllerScript.cs
@@ -1,30 +1,48 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class Car2DControllerScript : MonoBehaviour {
 
-	public float speedForce =100f;
-	public float torquqForce = 100f;
+    public float speedForce;
+    public float torqueForce;
 
-	// Use this for initialization
-	void Start () {
-	}
-	
-	// Update is called once per frame
-	void FixedUpdate () {
+    // Use this for initialization
+    void Start() {
+    }
 
-		Rigidbody2D rb = GetComponent<Rigidbody2D> ();
-		if (Input.GetButton ("Accelerate")) {
-			rb.AddForce (transform.up * speedForce);
-		} else if (Input.GetButton ("Break")) {
-			rb.AddForce (-transform.up * speedForce);
-		}
+    // Update is called once per frame
+    void FixedUpdate() {
+        Rigidbody2D carRigidBody = this.GetComponent<Rigidbody2D>();
 
-		rb.AddTorque (Input.GetAxis ("Horizontal") * torquqForce);
+        //0 - nu driftează deloc, 1 - driftează la 90 de grade
+        float driftFactor = 1 - (carRigidBody.velocity.magnitude != 0 ? (float)Math.Pow(Math.Abs(Vector2.Dot(carRigidBody.velocity.normalized, new Vector2(transform.up.x, transform.up.y))), 2) : 1);
+        /*produsul scalar a doi vectori arată "cât de paraleli" sunt ei;
+        în cazul acesta produsul scalar dintre vectorul cu direcția pe care o înfruntă mașina
+        și vectorul cu direcția velocității este 0 când aceștia sunt perpendiculari (mașina driftează la 90 de grade)
+        sau 1 când sunt paraleli (mașina merge înainte fără să drifteze); scad rezultatul final din 1 ca să obțin
+        driftFactor*/
+        
+        //0 - mașina stă pe loc, 1 - virajul are randament maxim
+        float turnFactor = Math.Min(carRigidBody.velocity.magnitude / speedForce, 1);
+        
+        //înainte + înapoi
+        carRigidBody.AddForce(Input.GetAxis("Throttle") * speedForce * transform.up);
+        //aplică forță pozitivă pe direcția pe care o înfruntă mașina
+        
+        //reducerea vitezei în caz de drift
+        carRigidBody.AddForce((-driftFactor) * (speedForce / 2) * carRigidBody.velocity.normalized);
+        /*aplică forță negativă pe direcția velocității, NU pe direcția pe care o înfruntă mașina;
+        cu cât e driftFactoru mai mare, cu atât forța e mai mare*/
+        
+        //stânga + dreapta
+        carRigidBody.AddTorque(turnFactor * Input.GetAxis("Steering") * torqueForce);
 
+        //oprirea rotației când mașina stă pe loc
+        carRigidBody.angularVelocity = carRigidBody.velocity.magnitude < 0.5 ? 0 : carRigidBody.angularVelocity;
+        //când viteza mașinii scade sub o constantă oarecare (se alege să fie cât mai realist) viteza unghiulară e setată la 0
 
-
-
-	}
+        Debug.Log(carRigidBody.velocity.magnitude);
+    }
 }
