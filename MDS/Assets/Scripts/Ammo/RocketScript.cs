68b1334defb662d9b6cad8bfb7058b30749e9afc
Alex Badea <Alex-Badea@github.com>
2017-06-04 13:34:38 +0300

    am îmbunătățit trasul cu racheta un pic

---
+++ MDS/Assets/Scripts/Ammo/RocketScript.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+/**
+ * Își crește gradual viteza din momentul tragerii
+ * Explodează la impact
+ * IGNORĂ IMPACTUL CU PLAYERUL CARE A TRAS
+ * Are un lifespan de 10 secunde
+ * Enemy tracking(opțional)
+ */
+public class RocketScript : MonoBehaviour {
+
+    public Transform sourceWeapon;
+    public Sprite sprite;
+
+	// Use this for initialization
+	void Start () {
+        this.gameObject.transform.position = sourceWeapon.position;
+        this.gameObject.transform.rotation = sourceWeapon.rotation;
+        this.gameObject.transform.localScale = new Vector3(0.2f, 0.2f, 0);
+
+        SpriteRenderer sr = this.gameObject.AddComponent<SpriteRenderer>();
+        sr.sprite = sprite;
+        sr.sortingLayerName = "Foreground";
+
+        Rigidbody2D rb = this.gameObject.AddComponent<Rigidbody2D>();
+        rb.gravityScale = 0;
+        rb.AddForce(sourceWeapon.parent.GetComponent<Rigidbody2D>().velocity * 50);
+
+        BoxCollider2D bc = this.gameObject.AddComponent<BoxCollider2D>();
+        Physics2D.IgnoreCollision(sourceWeapon.parent.GetComponent<BoxCollider2D>(), bc);
+
+        Destroy(this.gameObject, 10f);
+    }
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+        this.GetComponent<Rigidbody2D>().AddForce(transform.up * 20);
+    }
+}
--- MDS/Assets/Scripts/GunShootScript.cs
+++ MDS/Assets/Scripts/GunShootScript.cs
@@ -5,29 +5,16 @@ using UnityEngine;
 public class GunShootScript : MonoBehaviour {
 
     public Sprite sprite;
-    private Component comp;
 
     void Start () {
-        comp = null;
 	}
 
-    void FixedUpdate() {
+    void Update() {
         if (Input.GetButtonDown("Fire")) {
-            GameObject ammo = new GameObject("Ammo");
-            SpriteRenderer sr = ammo.AddComponent<SpriteRenderer>();
-            sr.sprite = sprite;
-            sr.sortingLayerName = "Foreground";
-            ammo.transform.position = this.transform.position;
-            ammo.transform.rotation = this.transform.rotation;
-
-            Rigidbody2D rb = ammo.AddComponent<Rigidbody2D>();
-            comp = rb;
-            rb.gravityScale = 0;
-            rb.AddForce(transform.up * 500);
+            GameObject ammo = new GameObject("Rocket");
+            RocketScript rs = ammo.AddComponent<RocketScript>();
+            rs.sourceWeapon = this.gameObject.transform;
+            rs.sprite = sprite;
         }
-        if (comp != null)
-            Debug.Log(((Rigidbody2D)comp).velocity.magnitude);
-        Debug.Log("asd" + (transform.up * 200).magnitude);
     }
-
 }
