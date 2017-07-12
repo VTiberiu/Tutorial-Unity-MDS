5fb9e968fb7efd37bf8198191e0913f9c9ba75fa
Alex Badea <Alex-Badea@github.com>
2017-07-12 21:10:08 +0300

    mp local functional

---
+++ MDS/Assets/Scripts/Player/GunShootScript.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GunShootScript : MonoBehaviour {
+
+    public GameObject ammo;
+
+    void Start () {
+	}
+
+    public void Shoot() {
+        Instantiate(ammo, this.gameObject.transform, false);
+    }
+
+}
